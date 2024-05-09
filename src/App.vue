6bcf7131d301aaa95ff5ba0894ba3796d8b2cabd
Beatriz Barroso <beatriznb@goparity.com>
2024-05-09 23:01:03 +0100

    functional vertical carousel

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script lang="ts">
 // import HomeMenu from './components/HomeMenu.vue'
 import { defineComponent } from 'vue'
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 
 export default defineComponent({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,139 @@
+<template>
+  <swiper
+    :direction="'vertical'"
+    :slidesPerView="1"
+    :spaceBetween="30"
+    :mousewheel="true"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide> 
+      <div id="home" class="page lg:grid lg:grid-cols-2 lg:justify-items-start h-fit">
+        <div class="w-fit">
+          <div class="font-extrabold w-fit">
+            <h3 style="font-family: Playfair, serif;"
+              class="typing-effect typing-effect__line1 text-6xl lg:text-9xl text-green-200">
+              Beatriz
+            </h3>
+          </div>
+          <div class="font-extrabold">
+            <h3 style="font-family: Playfair, serif;"
+              class="typing-effect typing-effect__line2 text-6xl lg:text-9xl text-green-200 m-0 p-0">
+              Barroso.
+            </h3>
+            <p class="font-extralight text-white tracking-widest text-2xl mt-2">Full-Stack Developer</p>
+          </div>
+        </div>
+        <div class="place-self-center">
+          <div class="relative w-full">
+            <div>
+              <img src="../assets/images/beatriz.png" alt="My photo"
+                class="rounded-[50%/60%_60%_60%_60%] drop-shadow-[0px_15px_3px_rgba(0,0,0,0.25)] opacity-20">
+            </div>
+            <div class="absolute bg-green-200 h-12 top-1/2 w-full rotate-45 opacity-30"></div>
+          </div>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div id="about" class="page">
+        <div class="lg:flex">
+          <div class="w-full">
+            <h2 class="mb-4">About</h2>
+            <div class="grid grid-cols-1 lg:grid-cols-2 font-extralight">
+              <div>
+                <p>
+                  Hello 👋 My name is a Beatriz and I'm a self-made developer.
+                </p>
+                <br>
+                <p>
+                  So, picture this: I was knee-deep in the world of theatre, soaking up the drama and creativity, when I
+                  stumbled upon a coding bootcamp. Crazy, right? But that's where the magic happened.
+                </p>
+                <br>
+                <p>
+                  Coding wasn't even on my radar, but once I dipped my toes into it, I was hooked. It was like
+                  discovering
+                  a whole new way to express myself, just with lines of code instead of lines on a stage.
+                </p>
+                <br>
+                <p>
+                  Fast forward a bit, and I found myself at Goparity, this awesome tech company with a mission I could
+                  really get behind. For the past five years, I've been pouring my heart and soul into our work, from
+                  coding to managing a team.
+                </p>
+                <br>
+                <p>
+                  Now, as the CTO, I'm juggling a bunch of responsibilities, but I'm still getting my hands dirty with
+                  code. I mean, you can take the person out of coding, but you can't take coding out of the person,
+                  right?
+                </p>
+                <br>
+                <p>
+                  I'm all about pushing boundaries and learning new stuff. With technology evolving faster than you can
+                  say "bug fix," I'm constantly on the lookout for the next big thing.
+                </p>
+                <br>
+                <p>
+                  So yeah, that's my journey – from theatre buff to coding wizard. Who knows what's next? But I'm ready
+                  for whatever comes my way.
+                </p>
+              </div>
+              <div class="place-self-center mt-8 lg:mt-0">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="page">
+        <h2 class="mb-8">Skill Set</h2>
+        <div class="grid grid-cols-2 lg:grid-cols-7 gap-y-8 gap-x-2 place-items-center">
+          <div v-for="(value, key, index) in skills" :key="index" class="h-24">
+            <img :src="value.url" class="h-14 hover:scale-125 transition-transform" @mouseover="visible = index"
+              @mouseout="visible = null">
+            <div v-if="visible === index" class="mt-4">
+              <p class="text-xs text-gray-100">skill level</p>
+              <div class="meter my-1 rounded-r-lg">
+                <span :style="'width:'+value.progress+'%;'">
+                  <span class="progress" :style="'background-color:' + value.color "></span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div id="contact" class="lg:grid lg:grid-rows-1 lg:justify-items-center page">
+        <h3 class="m-2">Looking forward to hearing from you!</h3>
+        <div class="flex items-center justify-center">
+          <a href="mailto:beatrizscnbarroso@gmail.com" target="_blank" class="mx-2">
+            <img src="../assets/images/email.png" alt="Email logo" class="h-10">
+          </a>
+          <a href="https://www.linkedin.com/in/beatriz-barroso-031905124/" target="_blank" class="mx-2">
+            <img src="../assets/images/linkedin.png" alt="Linkedin logo" class="h-10">
+          </a>
+          <a href="https://github.com/beatriznbarroso" target="_blank" class="mx-2">
+            <img src="../assets/images/github.png" alt="Github logo" class="h-10">
+          </a>
+        </div>
+      </div> 
+    </swiper-slide>
+  </swiper>
+</template>
+
 <script lang="ts">
 import { ref } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Pagination, Scrollbar } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Mousewheel, Pagination } from 'swiper/modules';
 
 export default {
   components: {
@@ -140,147 +272,15 @@ export default {
       },
     })
 
+
     return {
-      modules: [Pagination, Scrollbar],
+      modules: [Mousewheel, Pagination ],
       skills,
       visible
     };
   },
 };
 </script>
-
-<template>
-  <main class="scroll-smooth">
-    <div>
-      <swiper
-        :pagination="{
-          clickable: true,
-        }"
-        :direction="'vertical'"
-        :mousewheel="true"
-        :auto-height="true"
-        space-between="10"
-        :modules="modules"
-      >
-        <swiper-slide> 
-          <div id="home" class="page lg:grid lg:grid-cols-2 lg:justify-items-start h-fit">
-            <div class="w-fit">
-              <div class="font-extrabold w-fit">
-                <h3 style="font-family: Playfair, serif;"
-                  class="typing-effect typing-effect__line1 text-6xl lg:text-9xl text-green-200">
-                  Beatriz
-                </h3>
-              </div>
-              <div class="font-extrabold">
-                <h3 style="font-family: Playfair, serif;"
-                  class="typing-effect typing-effect__line2 text-6xl lg:text-9xl text-green-200 m-0 p-0">
-                  Barroso.
-                </h3>
-                <p class="font-extralight text-white tracking-widest text-2xl mt-2">Full-Stack Developer</p>
-              </div>
-            </div>
-            <div class="place-self-center">
-              <div class="relative w-full">
-                <div>
-                  <img src="../assets/images/beatriz.png" alt="My photo"
-                    class="rounded-[50%/60%_60%_60%_60%] drop-shadow-[0px_15px_3px_rgba(0,0,0,0.25)] opacity-20">
-                </div>
-                <div class="absolute bg-green-200 h-12 top-1/2 w-full rotate-45 opacity-30"></div>
-              </div>
-            </div>
-          </div>
-        </swiper-slide>
-        <swiper-slide>
-          <div id="about" class="page">
-            <div class="lg:flex">
-              <div class="w-full">
-                <h2 class="mb-4">About</h2>
-                <div class="grid grid-cols-1 lg:grid-cols-2 font-extralight">
-                  <div>
-                    <p>
-                      Hello 👋 My name is a Beatriz and I'm a self-made developer.
-                    </p>
-                    <br>
-                    <p>
-                      So, picture this: I was knee-deep in the world of theatre, soaking up the drama and creativity, when I
-                      stumbled upon a coding bootcamp. Crazy, right? But that's where the magic happened.
-                    </p>
-                    <br>
-                    <p>
-                      Coding wasn't even on my radar, but once I dipped my toes into it, I was hooked. It was like
-                      discovering
-                      a whole new way to express myself, just with lines of code instead of lines on a stage.
-                    </p>
-                    <br>
-                    <p>
-                      Fast forward a bit, and I found myself at Goparity, this awesome tech company with a mission I could
-                      really get behind. For the past five years, I've been pouring my heart and soul into our work, from
-                      coding to managing a team.
-                    </p>
-                    <br>
-                    <p>
-                      Now, as the CTO, I'm juggling a bunch of responsibilities, but I'm still getting my hands dirty with
-                      code. I mean, you can take the person out of coding, but you can't take coding out of the person,
-                      right?
-                    </p>
-                    <br>
-                    <p>
-                      I'm all about pushing boundaries and learning new stuff. With technology evolving faster than you can
-                      say "bug fix," I'm constantly on the lookout for the next big thing.
-                    </p>
-                    <br>
-                    <p>
-                      So yeah, that's my journey – from theatre buff to coding wizard. Who knows what's next? But I'm ready
-                      for whatever comes my way.
-                    </p>
-                  </div>
-                  <div class="place-self-center mt-8 lg:mt-0">
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </swiper-slide>
-        <swiper-slide>
-          <div class="page">
-            <h2 class="mb-8">Skill Set</h2>
-            <div class="grid grid-cols-2 lg:grid-cols-7 gap-y-8 gap-x-2 place-items-center">
-              <div v-for="(value, key, index) in skills" :key="index" class="h-24">
-                <img :src="value.url" class="h-14 hover:scale-125 transition-transform" @mouseover="visible = index"
-                  @mouseout="visible = null">
-                <div v-if="visible === index" class="mt-4">
-                  <p class="text-xs text-gray-100">skill level</p>
-                  <div class="meter my-1 rounded-r-lg">
-                    <span :style="'width:'+value.progress+'%;'">
-                      <span class="progress" :style="'background-color:' + value.color "></span>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </swiper-slide>
-        <swiper-slide>
-          <div id="contact" class="lg:grid lg:grid-rows-1 lg:justify-items-center page">
-            <h3 class="m-2">Looking forward to hearing from you!</h3>
-            <div class="flex items-center justify-center">
-              <a href="mailto:beatrizscnbarroso@gmail.com" target="_blank" class="mx-2">
-                <img src="../assets/images/email.png" alt="Email logo" class="h-10">
-              </a>
-              <a href="https://www.linkedin.com/in/beatriz-barroso-031905124/" target="_blank" class="mx-2">
-                <img src="../assets/images/linkedin.png" alt="Linkedin logo" class="h-10">
-              </a>
-              <a href="https://github.com/beatriznbarroso" target="_blank" class="mx-2">
-                <img src="../assets/images/github.png" alt="Github logo" class="h-10">
-              </a>
-            </div>
-          </div> 
-        </swiper-slide>
-      </swiper>
-    </div>
-  </main>
-</template>
 
 <style lang="scss" scoped>
 .typing-effect {
