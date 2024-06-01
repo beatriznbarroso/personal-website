8b29d873e125f719a78bca54624badc951e63789
Beatriz Barroso <beatrizbarroso@BeatrizsMacbook.Home>
2024-06-01 16:50:22 +0100

    fixing unused routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
-import Projects from '../views/ProjectsView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: Projects
     }
   ]
 })
