508f0b02fa0d433295fbb57900e922d0d105ea12
Beatriz Barroso <beatrizbarroso@BeatrizsMacbook.Home>
2024-10-27 23:08:13 +0000

    fixing tablet screens

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,25 +6,25 @@
       dynamicBullets: true
     }" :modules="modules">
     <swiper-slide class="mt-8">
-      <div class="page lg:grid lg:grid-cols-2 lg:justify-items-start h-fit">
+      <div class="page xl:grid xl:grid-cols-2 xl:justify-items-start h-fit">
         <div class="w-fit lg:pt-24">
           <div class="font-extrabold w-fit">
             <h3 style="font-family: Playfair, serif;"
-              class="typing-effect typing-effect__line1 text-6xl lg:text-9xl text-green-200">
+              class="typing-effect typing-effect__line1 text-6xl md:text-9xl text-green-200">
               Beatriz
             </h3>
           </div>
-          <div class="font-extrabold">
+          <div class="font-extrabold w-fit">
             <h3 style="font-family: Playfair, serif;"
-              class="typing-effect typing-effect__line2 text-6xl lg:text-9xl text-green-200 m-0 p-0">
+              class="typing-effect typing-effect__line2 text-6xl md:text-9xl text-green-200 m-0 p-0">
               Barroso.
             </h3>
-            <p class="font-extralight text-white tracking-widest text-2xl mt-2">Full-Stack Developer</p>
+            <p class="font-extralight text-white tracking-widest text-2xl md:text-4xl mt-2">Full-Stack Developer</p>
           </div>
         </div>
         <div class="w-full">
-          <div class="flex items-center justify-start lg:justify-end mt-2 lg:mt-0">
-            <svg class="hidden lg:block w-32 animate-pulse" viewBox="0 0 210 80" fill="none"
+          <div class="flex items-center justify-start xl:justify-end mt-2 lg:mt-0">
+            <svg class="hidden xl:block w-32 animate-pulse" viewBox="0 0 210 80" fill="none"
               xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M192.125 58.2413C192.192 57.6167 192.752 57.1644 193.377 57.231C196.667 57.5819 199.545 57.2548 202.945 56.7976C202.942 56.7979 202.941 56.7981 202.941 56.7981C202.941 56.798 202.955 56.7958 202.996 56.7888L203.172 56.7587C203.317 56.7342 203.511 56.7018 203.733 56.6677C204.171 56.6004 204.739 56.5229 205.258 56.4872C205.516 56.4695 205.786 56.4602 206.035 56.4725C206.242 56.4827 206.59 56.512 206.905 56.6644C207.074 56.7459 207.353 56.9205 207.526 57.2657C207.718 57.6504 207.674 58.0347 207.568 58.3079C207.473 58.5517 207.323 58.7313 207.213 58.8456C207.095 58.9689 206.96 59.0802 206.823 59.1805C204.926 60.576 202.91 61.7866 200.924 62.953C200.68 63.0965 200.436 63.2392 200.193 63.3815C198.446 64.4047 196.736 65.4064 195.101 66.5097C194.909 66.6391 193.976 67.3372 193.084 68.0272C192.644 68.3671 192.233 68.6909 191.948 68.9263C191.848 69.0081 191.772 69.0723 191.719 69.1191C191.516 69.4006 191.188 69.5861 190.814 69.5921C190.707 69.5938 190.327 69.5872 189.999 69.2714C189.616 68.9011 189.633 68.4425 189.669 68.243C189.704 68.0519 189.78 67.9166 189.808 67.8688C189.844 67.8059 189.88 67.7578 189.902 67.7295C189.977 67.6327 190.063 67.5514 190.103 67.5142C190.207 67.4169 190.349 67.2958 190.502 67.1703C190.815 66.9126 191.247 66.572 191.692 66.2281C192.568 65.5504 193.564 64.803 193.829 64.6242C195.528 63.4772 197.306 62.4364 199.046 61.417C199.289 61.2748 199.531 61.1331 199.772 60.9916C200.835 60.3673 201.881 59.7449 202.899 59.0986C199.625 59.5345 196.599 59.8623 193.136 59.4928C192.511 59.4262 192.059 58.8659 192.125 58.2413ZM191.642 69.1894C191.642 69.1895 191.644 69.1879 191.647 69.1844C191.644 69.1876 191.642 69.1894 191.642 69.1894Z"
@@ -33,21 +33,21 @@
                 d="M114.43 2.97243C116.799 5.09212 118.387 8.57955 118.626 11.6136C119.353 20.8227 114.274 30.3893 110.082 37.5959C106.752 43.3198 103.611 48.0292 99.5294 52.5712C101.02 57.4027 103.248 61.7107 106.222 65.2931C111.327 71.4422 118.694 75.5312 128.533 76.4329C148.657 78.2771 167.237 71.1229 185.832 63.9626C189.04 62.7276 192.248 61.4923 195.464 60.303C196.053 60.0852 196.707 60.3862 196.925 60.9754C197.143 61.5645 196.842 62.2187 196.253 62.4365C193.096 63.6039 189.927 64.8257 186.741 66.054C168.168 73.2145 149.025 80.595 128.325 78.698C117.92 77.7445 109.985 73.3864 104.472 66.7461C101.541 63.2164 99.3129 59.064 97.7547 54.4735C96.7891 55.4712 95.7697 56.4665 94.6845 57.4686C82.9357 68.3175 68.3983 71.6042 53.5183 65.2723C38.3935 58.8362 25.1024 47.5198 13.4406 36.456C8.51966 31.7874 4.35167 27.2421 1.13205 21.1509C0.83852 20.5956 1.05075 19.9074 1.60608 19.6139C2.16141 19.3204 2.84955 19.5326 3.14308 20.0879C6.19258 25.8573 10.1539 30.2024 15.0061 34.8058C26.6399 45.8429 39.6831 56.9129 54.409 63.1793C68.3846 69.1263 81.9831 66.101 93.1414 55.7974C94.524 54.5207 95.7963 53.2557 96.986 51.9807C96.0653 48.6755 95.4677 45.1732 95.1823 41.5364C94.7396 35.8933 94.7537 28.3549 96.0877 21.1916C97.4152 14.0631 100.087 7.1043 105.127 2.85747C106.751 1.4886 108.452 0.902528 110.12 1.01313C111.766 1.12224 113.23 1.89924 114.43 2.97243ZM98.8052 49.9497C98.1356 47.2197 97.6844 44.3451 97.45 41.3584C97.0177 35.8486 97.0367 28.52 98.3239 21.608C99.6176 14.6612 102.155 8.33627 106.592 4.59688C107.847 3.53943 108.985 3.21757 109.97 3.28282C110.976 3.34954 111.979 3.83217 112.913 4.66778C114.811 6.36568 116.161 9.29253 116.359 11.7926C117.022 20.1938 112.353 29.1675 108.115 36.4522C105.112 41.6154 102.295 45.8889 98.8052 49.9497Z"
                 fill="white" stroke="white" stroke-linecap="round"></path>
             </svg>
-            <a href="mailto:beatrizscnbarroso@gmail.com" target="_blank" class="mx-2 w-10">
+            <a href="mailto:beatrizscnbarroso@gmail.com" target="_blank" class="mx-2 w-10 md:w-14">
               <img src="../assets/images/email.png" alt="Email logo">
             </a>
-            <a href="https://www.linkedin.com/in/beatriz-barroso-031905124/" target="_blank" class="mx-2 w-10">
+            <a href="https://www.linkedin.com/in/beatriz-barroso-031905124/" target="_blank" class="mx-2 w-10 md:w-14">
               <img src="../assets/images/linkedin.png" alt="Linkedin logo">
             </a>
-            <a href="https://github.com/beatriznbarroso" target="_blank" class="mx-2 w-10">
+            <a href="https://github.com/beatriznbarroso" target="_blank" class="mx-2 w-10 md:w-14">
               <img src="../assets/images/github.png" alt="Github logo">
             </a>
           </div>
           <div>
-            <div class="w-96">
+            <div class="w-96 md:w-140 lg:w-176 xl:w-96">
               <div class="relative">
                 <img src="../assets/images/beatriz.png" alt="My photo"
-                  class="rounded-[50%/60%_60%_60%_60%] drop-shadow-[0px_15px_3px_rgba(0,0,0,0.25)] opacity-40 my-photo">
+                  class="rounded-[50%/60%_60%_60%_60%] drop-shadow-[0px_15px_3px_rgba(0,0,0,0.25)] opacity-40 my-photo bg-blend-hard-light">
               </div>
             </div>
           </div>
@@ -58,12 +58,12 @@
       <div class="page mt-4">
         <div class="xl:flex">
           <div class="w-full">
-            <h2 class="about mb-4 lg:mb-0" :class="reveal ? 'block' : 'hidden'">About <span
+            <h2 class="about mb-4 md:mb-24 lg:mb-0" :class="reveal ? 'block' : 'hidden'">About <span
                 class="lg:hidden about-me">Me</span></h2>
             <h2 class="hidden lg:block mb-8 about-me" :class="reveal ? 'block' : 'hidden'">Me</h2>
             <div class="grid grid-cols-1 xl:grid-cols-2 items-start">
               <div :class="{'reveal-text': reveal }" class="overflow-hidden">
-                <p class="font-extralight">
+                <p class="font-extralight text-sm md:text-2xl lg:text-3xl xl:text-lg">
                   Hello 👋 My name is a Beatriz and I'm a self-made developer.
                   <br />
                   I have always been a massive tech-geek and when I came across programming I knew I couldn't turn back.
@@ -78,7 +78,7 @@
                 </p>
               </div>
               <img src="../assets/images/xero-code.gif" alt="Animation"
-                class="!object-contain !h-2/5 xl:!h-3/6 lg:mt-[-70px] overflow-hidden"
+                class="!object-contain !h-2/5 xl:!h-3/6 overflow-hidden md:mt-20 lg:mt-32 xl:mt-[-70px] "
                 :class="{ 'reveal-text': reveal }">
             </div>
           </div>
@@ -87,9 +87,9 @@
     </swiper-slide>
     <swiper-slide>
       <div class="page">
-        <h2 class="mb-8">Skill Set</h2>
+        <h2 class="mb-4 md:mb-8">Skill Set</h2>
         <div
-          class="grid grid-cols-3 md:grid-cols-3 xl:grid-cols-7 gap-y-4 md:gap-y-12 lg:gap-y-16 xl:gap-y-8 gap-x-2 place-items-center">
+          class="grid grid-cols-3 md:grid-cols-3 xl:grid-cols-7 gap-y-4 md:gap-y-8 lg:gap-y-16 xl:gap-y-8 gap-x-2 place-items-center">
           <div v-for="(value, key, index) in skills" :key="index" class="h-16 md:h-24">
             <img :src="value['grey_url']" class="!object-cover hover:scale-125 transition-transform"
               @mouseover="visible = index" @mouseout="visible = null">
@@ -108,24 +108,27 @@
     <swiper-slide>
       <div>
         <h2 class="ml-4 lg:ml-32 2xl:ml-64">Projects</h2>
-        <div class="mt-16 lg:mt-4">
+        <div class="mt-16 md:mt-40 xl:mt-4">
           <swiper :effect="'coverflow'" :grabCursor="true" :centeredSlides="true" :slidesPerView="3" :initialSlide="1"
             :spaceBetween="50" :loop="true" @swiper="setSecondSwiper" @slideChange="onSecondSwiperSlideChange"
+            :autoHeight="true"
             :autoplay="{
               delay: 1500,
               disableOnInteraction: true,
-            }" :coverflowEffect="{
+            }" 
+            :coverflowEffect="{
               rotate: 30,
               stretch: 0,
               depth: 100,
               modifier: 1,
               slideShadows: true,
-            }" :breakpoints=" { 
+            }" 
+            :breakpoints=" { 
               320: {
                 slidesPerView: 1.2,
                 spaceBetween: 20
               },
-              640: {
+              1025: {
                 slidesPerView: 2,
                 spaceBetween: 50
               }
@@ -462,11 +465,6 @@ export default {
   text-underline-offset: 0.4em;
 }
 
-// .my-photo {
-//   transform: translatey(0px);
-//   animation: float 3s ease-in-out infinite;
-// }
-
 .reveal-text {
   display: hidden;
   animation: reveal 1.5s;
@@ -538,18 +536,4 @@ export default {
     margin-left: 40%;
   }
 }
-
-// @keyframes float {
-//   0% {
-//     transform: translatey(0px);
-//   }
-
-//   50% {
-//     transform: translatey(-20px);
-//   }
-
-//   100% {
-//     transform: translatey(0px);
-//   }
-// }
 </style>
