5182f1de6e62a1a57c82e7c7ad2ee443332ffa32
Beatriz Barroso <beatrizbarroso@Beatriz-s-Macbook.local>
2025-03-31 00:32:14 +0100

    added favicon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script lang="ts">
-// import HomeMenu from './components/HomeMenu.vue'
 import { defineComponent } from 'vue'
 
 
 export default defineComponent({
   components: {
-    // HomeMenu,
   }
 })
 </script>
 
 <template>
-  <!-- <HomeMenu /> -->
   <RouterView />
 </template>
 
